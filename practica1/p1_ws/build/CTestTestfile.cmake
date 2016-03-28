18fe09d236de90ac545d1b27e130a90b9c8e2e24
IsmaKepimKepam <ros@ros-xubuntu.(none)>
2016-03-28 20:26:47 +0200

    rm devel

--- practica1/p1_ws/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ros/VAR/practica1/p1_ws/src
-# Build directory: /home/ros/VAR/practica1/p1_ws/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
-SUBDIRS(p1_pkg)
-SUBDIRS(listener)
--- practica1/p1_ws/build/listener/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ros/VAR/practica1/p1_ws/src/listener
-# Build directory: /home/ros/VAR/practica1/p1_ws/build/listener
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
